414c647f9b89bbd16c47cda12b5ecdae56f5cb5e
daanmichiels <michielsdaan@gmail.com>
2014-07-26 16:33:57 -0500

    Convert shader source to std::string

--- src/shaders.cpp
+++ src/shaders.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 
 // Shaders will be linked to these
 extern char source_vs_start[];
@@ -11,17 +12,13 @@ extern char source_fs_size[];
 
 // Just testing whether the shaders get embedded correctly
 void print_shader_source() {
-	std::cout << "------- VERTEX SHADER -------\n";
-	for(char* p = source_vs_start; p < source_vs_end; p++)
-	{
-		std::cout << *p;
-	}
-	std::cout << "\n\n------ FRAGMENT SHADER ------\n";
-	for(char* p = source_fs_start; p < source_fs_end; p++)
-	{
-		std::cout << *p;
-	}
+	std::string source_vs(source_vs_start, (int)source_vs_size);
+	std::string source_fs(source_fs_start, (int)source_fs_size);
 
-	std::cout << ((int)source_vs_size);
+	std::cout << "------- VERTEX SHADER -------\n";
+	std::cout << source_vs << "\n";
+
+	std::cout << "\n\n------ FRAGMENT SHADER ------\n";
+	std::cout << source_fs << "\n";
 }
 

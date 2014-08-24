e4c9aee97305ac65715b605edd13dd229e8a54ee
daanmichiels <michielsdaan@gmail.com>
2014-08-24 16:10:19 -0500

    Reorganize primitives

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -4,29 +4,43 @@
 #include "primitives.h"
 #include "hypermath.h"
 
-// This file needs cleaning up.
-// A lot of code occurs multiple times. This should be separated in
-// simple functions.
-
-namespace primitives
+// Anonymous namespace.
+// Everything in here can only be accessed from this file (unit).
+namespace
 {
-    mesh triangle(glm::vec4 a, glm::vec4 b, glm::vec4 c)
+    const glm::vec4 red = glm::vec4(1.0f,0.0f,0.0f,1.0f);
+    const glm::vec4 green = glm::vec4(0.0f,1.0f,0.0f,1.0f);
+    const glm::vec4 blue = glm::vec4(0.0f,0.0f,1.0f,1.0f);
+    const glm::vec4 yellow = glm::vec4(1.0f,1.0f,0.0f,1.0f);
+
+    // Create a vao from positions and colors.
+    GLuint vao_from_pos_col(std::vector<glm::vec4> pos, std::vector<glm::vec4> col)
     {
+        size_t n = pos.size();
+        if(col.size() != n) {
+            return 0;
+        }
+
         GLuint vao;
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
 
-        // interleaved vertex coordinates and colors
         GLuint buffer;
         glGenBuffers(1, &buffer);
-        float data[] = {a.x, a.y, a.z, a.w,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            b.x, b.y, b.z, b.w,
-            0.0f, 1.0f, 0.0, 1.0f,
-            c.x, c.y, c.z, c.w,
-            0.0f, 0.0f, 1.0f, 1.0f};
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+        float* data = new float[8*n];
+        for(size_t i=0; i<n; i++) {
+            data[8*i+0] = pos[i].x;
+            data[8*i+1] = pos[i].y;
+            data[8*i+2] = pos[i].z;
+            data[8*i+3] = pos[i].w;
+            data[8*i+4] = col[i].r;
+            data[8*i+5] = col[i].g;
+            data[8*i+6] = col[i].b;
+            data[8*i+7] = col[i].a;
+        }
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float)*8*n, data, GL_STATIC_DRAW);
+        delete [] data;
 
         glEnableVertexAttribArray(0);
         glEnableVertexAttribArray(1);
@@ -36,8 +50,18 @@ namespace primitives
         glBindVertexArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+        return vao;
+    }
+}
+
+namespace primitives
+{
+    // Create a triangle.
+    // The corners will be red, green, blue.
+    mesh triangle(glm::vec4 a, glm::vec4 b, glm::vec4 c)
+    {
         mesh result;
-        result.vao = vao;
+        result.vao = vao_from_pos_col({a,b,c},{red,green,blue});
         result.mode = GL_TRIANGLES;
         result.first = 0;
         result.count = 3;
@@ -49,45 +73,15 @@ namespace primitives
     // It's uniformly colored.
     mesh rectangle(float width, float depth, glm::vec4 color)
     {
-        // interleaved vertex coordinates and colors
-        float data[32];
         float w = width / 2;
         float d = depth / 2;
-        glm::vec4 points[4] = {hypermath::exp0(glm::vec4(-w,0, d,0)), \
+        std::vector<glm::vec4> pos = {hypermath::exp0(glm::vec4(-w,0, d,0)), \
             hypermath::exp0(glm::vec4( w,0, d,0)), \
             hypermath::exp0(glm::vec4(-w,0,-d,0)), \
             hypermath::exp0(glm::vec4( w,0,-d,0))};
-        for(int i=0; i<4; i++)
-        {
-            data[8*i+0] = points[i].x;
-            data[8*i+1] = points[i].y;
-            data[8*i+2] = points[i].z;
-            data[8*i+3] = points[i].w;
-            data[8*i+4] = color.r;
-            data[8*i+5] = color.g;
-            data[8*i+6] = color.b;
-            data[8*i+7] = color.a;
-        }
-
-        GLuint vao;
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-
-        GLuint buffer;
-        glGenBuffers(1, &buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(data), &data[0], GL_STATIC_DRAW);
-
-        glEnableVertexAttribArray(0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8*sizeof(GL_FLOAT), 0);
-        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 8*sizeof(GL_FLOAT), (void*)(4*sizeof(GL_FLOAT)));
-
-        glBindVertexArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
         mesh result;
-        result.vao = vao;
+        result.vao = vao_from_pos_col(pos, {color,color,color,color});
         result.mode = GL_TRIANGLE_STRIP;
         result.first = 0;
         result.count = 4;
@@ -111,45 +105,8 @@ namespace primitives
         c = hypermath::exp0(c);
         d = hypermath::exp0(d);
 
-        glm::vec4 red = glm::vec4(1.0f,0.0f,0.0f,1.0f);
-        glm::vec4 green = glm::vec4(0.0f,1.0f,0.0f,1.0f);
-        glm::vec4 blue = glm::vec4(0.0f,0.0f,1.0f,1.0f);
-        glm::vec4 yellow = glm::vec4(1.0f,1.0f,0.0f,1.0f);
-
-        glm::vec4 points[6] = {a,b,c,d,a,b};
-        glm::vec4 colors[6] = {red,green,blue,yellow,red,green};
-        float data[48];
-        for(int i=0; i<6; i++)
-        {
-            data[8*i+0] = points[i].x;
-            data[8*i+1] = points[i].y;
-            data[8*i+2] = points[i].z;
-            data[8*i+3] = points[i].w;
-            data[8*i+4] = colors[i].r;
-            data[8*i+5] = colors[i].g;
-            data[8*i+6] = colors[i].b;
-            data[8*i+7] = colors[i].a;
-        }
-
-        GLuint vao;
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-
-        GLuint buffer;
-        glGenBuffers(1, &buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(data), &data[0], GL_STATIC_DRAW);
-
-        glEnableVertexAttribArray(0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8*sizeof(GL_FLOAT), 0);
-        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 8*sizeof(GL_FLOAT), (void*)(4*sizeof(GL_FLOAT)));
-
-        glBindVertexArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
         mesh result;
-        result.vao = vao;
+        result.vao = vao_from_pos_col({a,b,c,d,a,b},{red,green,blue,yellow,red,green});
         result.mode = GL_TRIANGLE_STRIP;
         result.first = 0;
         result.count = 6;

c5588f68317c72cf6913f56c33f4d3473b2b8bff
dpugliese36 <d.pugs6@gmail.com>
2014-09-29 16:11:43 -0500

    Delete fragments.h
    removing fragments.h - with the new Makefile it will be generated by the make command

--- src/shaders/fragments.h
+++
@@ -1,6 +0,0 @@
-#ifndef FRAGMENT_SHADER
-#define FRAGMENT_SHADER
-
-char _binary_src_shaders_fragment_glsl_start[] = "#version 330\n\nsmooth in vec3 theColor;\n\nout vec4 finalColor;\n\nvoid main() {\n  finalColor = vec4(theColor, 0.5);\n}";
-
-#endif

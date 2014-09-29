2730698cb412216ab5728ee034dfb9b7e166e11b
dpugliese36 <d.pugs6@gmail.com>
2014-09-29 16:12:10 -0500

    Delete vertex.h
    removing vertex.h - with the new Makefile it will be generated by the make command

--- src/shaders/vertex.h
+++
@@ -1,6 +0,0 @@
-#ifndef VERTEX_SHADER
-#define VERTEX_SHADER
-
-char _binary_src_shaders_vertex_glsl_start[] = "#version 330\n\nlayout (location = 0) in vec4 position;\nlayout (location = 1) in vec3 color;\nuniform mat4 projection;\nuniform mat4 modelview;\n\nsmooth out vec3 theColor;\nvoid main()\n{\n  gl_Position = projection * modelview * position;\n  theColor = color;\n}";
-
-#endif

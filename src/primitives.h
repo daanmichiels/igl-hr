6755c22cd6211efd6c4614cbd4b276fb6c865233
dpugliese <d.pugs6@gmail.com>
2014-10-06 10:18:49 -0500

    Added a generic mesh maker which creates a mesh given any number of vertices (>= 3 of course)

--- src/primitives.h
+++ src/primitives.h
@@ -10,6 +10,7 @@ namespace primitives
     mesh triangle(glm::vec4 a, glm::vec4 b, glm::vec4 c);
     mesh rectangle(float width, float depth, glm::vec4 color);
     mesh tetrahedron(float radius);
+    mesh generic(std::vector<glm::vec4> v);
 }
 
 #endif

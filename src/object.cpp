c68ad85c6b9d1b9e7c24e1e77009e05ef4b3f2ed
dpugliese <d.pugs6@gmail.com>
2014-10-06 10:19:33 -0500

    Extending file loading constructor to allow for sides with any number of vertices (>= 3)

--- src/object.cpp
+++ src/object.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 #include <cmath>
 #include "primitives.h"
@@ -15,50 +16,51 @@ void object::transform(glm::mat4 transformation)
 
 object::object(const char filename[])
 {
-  std::ifstream input(filename);
+    std::ifstream input(filename);
 
-  std::vector<glm::vec4> vertices;
+    std::vector<glm::vec4> vertices;
 
-  int numVertices = 0;
+    int numVertices = 0;
 
-  for(std::string line; getline(input, line); ) {
-    if (line[0] == 'v') {
-      double x, y, z, w;
-      size_t index;
+    for(std::string line; getline(input, line); ) {
+        if (line[0] == 'v') {
+            double x, y, z, w;
+            size_t index;
 
-      line = line.substr(1);
+            line = line.substr(1);
 
-      x = std::stod(line, &index);
-      line = line.substr(index);
+            x = std::stod(line, &index);
+            line = line.substr(index);
 
-      y = std::stod(line, &index);
-      line = line.substr(index);
+            y = std::stod(line, &index);
+            line = line.substr(index);
 
-      z = std::stod(line);
-      line = line.substr(index);
+            z = std::stod(line);
+            line = line.substr(index);
 
-      w = std::stod(line);
+            w = std::stod(line);
 
-      vertices.push_back(glm::vec4(x, y, z, w));
-      numVertices++;
+            vertices.push_back(glm::vec4(x, y, z, w));
+            numVertices++;
+        }
+        else if (line[0] == 'f') {
+            std::stringstream linestream(line.substr(1));
+            std::vector<glm::vec4> v;
+
+            while(1) {
+                int i;
+
+                linestream >> i;
+                if (!linestream) {
+                    break;
+                }
+
+                v.push_back(vertices[i - 1]);
+            }
+
+            meshes.push_back(primitives::generic(v));
+        }
     }
-    else if (line[0] == 'f') {
-      int a, b, c;
-      size_t index;
-
-      line = line.substr(1);
-
-      a = std::stoi(line, &index) - 1;
-      line = line.substr(index);
-
-      b = std::stoi(line, &index) - 1;
-      line = line.substr(index);
-
-      c = std::stoi(line) - 1;
-
-      meshes.push_back(primitives::triangle(vertices[a], vertices[b], vertices[c]));
-    }
-  }
 }
 
 object::object()

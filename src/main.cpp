4d8267564534e68a66e3b288cb7a0157546206fd
daanmichiels <michielsdaan@gmail.com>
2014-10-10 19:15:56 -0500

    Create InputHandler

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include "mesh.h"
 #include "fpscounter.h"
 #include "cameracontrols.h"
+#include "inputhandler.h"
 
 #include "../thirdparty/glm/glm/glm.hpp"
 #include "../thirdparty/glm/glm/gtx/string_cast.hpp"
@@ -20,38 +21,10 @@
 #include "../thirdparty/glm/glm/gtx/transform.hpp"
 #include "../thirdparty/glm/glm/gtc/quaternion.hpp"
 
-CameraControls control = CameraControls(NULL, NULL);
 // Called on GLFW error.
 static void error_callback(int error, const char* description)
 {
     std::cerr << description;
-}
-
-// Bind ESC to window close 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-    if(key == GLFW_KEY_M && action == GLFW_PRESS)
-    {
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        double center_x = floor(width/2);
-        double center_y = floor(height/2);
-        glfwSetCursorPos(window, center_x, center_y);
-        control.bind_mouse = ! control.bind_mouse;
-        if(control.bind_mouse)
-        {
-            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-        }
-        if(!control.bind_mouse)
-        {
-            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-
-        }
-    }
 }
 
 // Creates the window and gets an OpenGL context for it.
@@ -72,12 +45,12 @@ GLFWwindow* create_window()
     window = glfwCreateWindow(800, 600, "Hyperbolic space on the Rift", NULL, NULL);
     if (!window)
     {
-        std::cout << "Failed to create window. Do you have OpenGL 3.0 or higher?\n";
+        std::cout << "Failed to create window. Do you have OpenGL 3.3 or higher?\n";
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, InputHandler::key_callback);
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
@@ -121,79 +94,35 @@ int main(int argc, const char* argv[])
         exit(EXIT_FAILURE);
     }
 
-    // create some meshes
-    glm::vec4 a = hypermath::exp0(glm::vec4(0.0f, 0.05f, 0.0f, 0.0f));
-    glm::vec4 b = hypermath::exp0(glm::vec4(0.05f, -0.05f, 0.0f, 0.0f));
-    glm::vec4 c = hypermath::exp0(glm::vec4(-0.05f, -0.05f, 0.0f, 0.0f));
-    mesh mesh_triangle = primitives::triangle(a,b,c);
-    mesh mesh_tetra    = primitives::tetrahedron(0.04);
-
-    // create some objects
-    object o1, o2, o3, t1, t2, t3;
-    o1.meshes.push_back(mesh_triangle);
-    o2.meshes.push_back(mesh_tetra);
-    o3.meshes.push_back(mesh_tetra);
-    glm::vec4 location1 = hypermath::exp0(glm::vec4(0.04,0,-0.1,0));
-    glm::vec4 location2 = hypermath::exp0(glm::vec4(0,0,-0.2,0));
-    glm::vec4 location3 = hypermath::exp0(glm::vec4(0,0,0.07,0));
-    o1.transform(hypermath::translation0(location1));
-    o2.transform(hypermath::translation0(location2));
-    o3.transform(hypermath::translation0(location3));
-
+    object t1, t2, t3;
     t1 = object(filename, false, 1);
     t2 = object(filename, false, 0.1);
     t3 = object(filename, false, 0.01);
-
-    // relations between the objects
-    o2.children.push_back(&o3);
 
     // create a camera
     Camera cam(1.2f, 800.0f/600.0f, 0.001f, 100.0f);
 
     // set up the scene
     Scene s = Scene();
-    // s.objects.push_back(&o1);
-    // s.objects.push_back(&o2);
     s.objects.push_back(&t1);
     s.objects.push_back(&t2);
     s.objects.push_back(&t3);
     s.camera = cam;
     s.program = program;
 
-    // make it more interesting
-    // object grid[240];
-    // int count = 0;
-    // for(int i=0; i<10; i++)
-    // {
-    //     float x = -0.2+0.04*i;
-    //     for(int j=0; j<6; j++)
-    //     {
-    //         float y = -0.2 + 0.07 * j;
-    //         for(int k=0; k<4; k++)
-    //         {
-    //             float z = -0.8 + k*0.1;
-    //             grid[count].meshes.push_back(mesh_tetra);
-    //             glm::vec4 location = hypermath::exp0(glm::vec4(x,y,z,0));
-    //             grid[count].transform(hypermath::translation0(location));
-    //             s.objects.push_back(&(grid[count]));
-    //             count++;
-    //         }
-    //     }
-    // }
-                
+    // set up camera controls
+    InputHandler::cameracontrols = CameraControls(window, &s.camera);
+
+    FpsCounter fps = FpsCounter(true);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
     //setup delta_time (must be outside of main loop)
     double current_time = glfwGetTime();
     double last_time = current_time;
     double delta_time;
-
-    FpsCounter fps = FpsCounter(true);
-    control = CameraControls(window, &s.camera);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-
-
+    // main loop
     while (!glfwWindowShouldClose(window))
     {
-        double t = glfwGetTime();
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
         float initialFoV = ((float)width)/height;
@@ -207,12 +136,6 @@ int main(int argc, const char* argv[])
         last_time = current_time;
         current_time = glfwGetTime();
         delta_time = current_time - last_time;
-        // obj movement
-        glm::mat4 rotation1 = glm::rotate((float)t,glm::vec3(0.0f,1.0f,0.0f));
-        glm::mat4 rotation2 = glm::rotate((float)(2*t),glm::vec3(0.0f,1.0f,0.0f));
-        location2 = hypermath::exp0(glm::vec4(0,0.1*sin(0.3*t),-0.2,0));
-        o1.transformation = rotation1 * hypermath::translation0(location1);
-        o2.transformation = hypermath::translation0(location2) * rotation2;
 
         glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -222,8 +145,7 @@ int main(int argc, const char* argv[])
         glfwSwapBuffers(window);
         glfwPollEvents();
 
-        /*Keyboard Mapping*/
-        control.handle(delta_time, width, height);
+        InputHandler::handle(delta_time, width, height);
 	
         fps.update(current_time);
 

330f2a64adafff13f8c430c0598e3b61e8f10446
daanmichiels <michielsdaan@gmail.com>
2014-12-21 11:56:20 -0600

    Basic rift detection in RiftManager

--- src/riftmanager/riftmanager.cpp
+++ src/riftmanager/riftmanager.cpp
@@ -1,8 +1,32 @@
 
 #include "riftmanager.h"
 #include "../logmanager/logmanager.h"
+#include "../configuration/configuration.h"
+#include "OVR/OVR.h"
+
+bool RiftManager::rift_active = false;
+ovrHmd RiftManager::rift = NULL;
 
 bool RiftManager::startup() {
+	if(Configuration::rift_input == OnOffAuto::off) {
+		rift_active = false;
+	} else {
+		ovrHmd rift = ovrHmd_Create(0);
+		if(!rift) {
+			if(Configuration::rift_input == OnOffAuto::on) {
+				LogManager::log_warning("No Rift detected. Using debug.", 1);
+				rift = ovrHmd_CreateDebug(ovrHmd_DK1);
+				rift_active = true;
+			} else {
+				LogManager::log_info("No Rift detected.", 1);
+				rift_active = false;
+			}
+		} else {
+			LogManager::log_info("Rift detected.", 1);
+			rift_active = true;
+		}
+	}
+
     LogManager::log_info("RiftManager started.", 2);
     return true;
 }
--- src/riftmanager/riftmanager.h
+++ src/riftmanager/riftmanager.h
@@ -1,10 +1,15 @@
 #ifndef RIFTMANAGER_H
 #define RIFTMANAGER_H
 
+#include "OVR/OVR.h"
+
 class RiftManager {
 public:
     static bool startup();
     static void shutdown();
+
+	static bool rift_active;
+	static ovrHmd rift;
 };
 
 #endif

0645111aeddb4efa16c83d9ba1e72651b640a390
daanmichiels <michielsdaan@gmail.com>
2015-01-02 12:30:51 -0600

    Remove unused code from scene.h

--- src/data/scene.h
+++ src/data/scene.h
@@ -11,17 +11,6 @@
 {
     public:
         std::vector<object*> objects;
-        GLuint program;
-        GLuint lens_center_loc;
-        GLuint barrel_power_loc;
-        // TODO: what is this?
-        bool _has_printed = false;
-        
-    private:
-        // what is this?
-        double _border = 0.1;
-        void render_object(object o, glm::dmat4 modelview);
-        void render_mesh(mesh m);
 };
 
 #endif /* SCENE_H */

880258b882ca8b37eba57a78934703c8f4a46f41
mcdaniel67 <mcdanie5@illinois.edu>
2015-02-10 21:29:10 -0600

    Finished doxygen style comments

--- src/data/object.cpp
+++ src/data/object.cpp
@@ -10,21 +10,35 @@
 #include "../math/primitives.h"
 #include "../math/hypermath.h"
 
+/** \brief Transform an object by a given 4x4 double transformation matrix
+ * \param 4x4 double transformation matrix
+ * \return void
+ */
 void object::transform(glm::dmat4 transformation)
 {
     this->transformation = transformation * this->transformation;
 }
 
+/** \brief Set the transformation matrix of an object to a given 4x4 double transformation matrix.
+ * \param 4x4 double transformation matrix.
+ * \return void
+ */
 void object::set_transformation(glm::dmat4 transformation)
 {
     this->transformation = transformation;
 }
 
+/** \brief Object constructor from a given mesh.
+ * \param Mesh for the object
+ */
 object::object(mesh object_mesh)
 {
     this->meshes.push_back(object_mesh);
 }
 
+/** \brief Empty object constructor
+ * \param void
+ */
 object::object()
 {
 

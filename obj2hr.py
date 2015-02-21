e73b6d7d385056ac8e85c8878e3b7208fef4f5b3
bwooden <barinskajones@comcast.net>
2015-02-20 18:07:42 -0600

    First draft of python script
    Python script took the suzy.obj file and made suzy.hr. Suzy filename was
    programmed in.

--- obj2hr.py
+++ obj2hr.py
@@ -1,14 +1,36 @@
-#"c:/users/byron/programming/py_4_notepad.bat" "c:/users/byron/oculus rift project/igl-hr/obj2hr.py"
-def exp0(x,y,z):
-	dot = x**2 + y**2 + z**2 - 1;
+#"c:/users/byron/programming/py_4_notepad.bat" "c:/users/byron/oculus rift project/igl-hr/" obj2hr.py
+#'''
+import math
+def exp0(dir):
+	basepoint = [0,0,0,1];
+	dot = sum([x**2 for x in dir]);
+	r = dot**(.5);
+	if r == 0:
+		return basepoint;
 	
+	return [ m+n for m,n in zip([i*math.cosh(r) for i in basepoint], [j*(math.sinh(r)/r) for j in dir]) ];
 obj = open("resources/suzy.obj","r");
+
 text = "";
+
 for line in obj:
-	if line[0] == "v"
+	if line[0] == "v":
 		parts = line.split(" ");
 		x=float(parts[1]);
 		y=float(parts[2]);
 		z=float(parts[3]);
-		
-		
+		vec=exp0([x,y,z,0]);
+		x=vec[0];
+		y=vec[1];
+		z=vec[2];
+		w=vec[3];
+		if len(text) > 0:
+			text += ",("+str(x)+","+str(y)+","+str(z)+","+str(w)+")";
+		else:
+			text += "("+str(x)+","+str(y)+","+str(z)+","+str(w)+")";
+			
+hr = open("resources/suzy.hr","w");
+hr.write(text);
+obj.close();
+hr.close();
+#'''

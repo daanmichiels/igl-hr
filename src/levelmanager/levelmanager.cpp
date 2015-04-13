b3b5683a692fcfbb4efac8e1757c45a6e31756f3
mcdaniel67 <mcdanie5@illinois.edu>
2015-04-12 23:09:50 -0500

    Added level parser. See resources/Levels/Level1.lev for an example.

--- src/levelmanager/levelmanager.cpp
+++ src/levelmanager/levelmanager.cpp
@@ -7,8 +7,14 @@
 #include "../assetmanager/assetmanager.h"
 #include "../data/object.h"
 #include "../math/tilings.h"
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
 #include <cmath>
-
+#include "glm/glm.hpp"
+#include "glm/gtx/string_cast.hpp"
 Scene LevelManager::scene = Scene();
 
 /** \brief Startup LevelManager. Logs LevelManager started at level 2
@@ -34,8 +40,6 @@ void LevelManager::shutdown() {
  * \return void
  */
 void LevelManager::load_initial_level() {
-    const double PI = 3.141592653589793238463;
-
     //-------------
 
     mesh axe_mesh = primitives::axes(10.0);
@@ -45,16 +49,12 @@ void LevelManager::load_initial_level() {
 
     //-------------
 
-    mesh tile = tilings::generate_tiling(3, 12, 6, 1.5);
-    object* tiling = new object(tile);
-    scene.objects.push_back(tiling);
-
-	LogManager::log_info("Enjoy Your Pi", 2);
-
+    load_level("Level1");
     return;
 }
 
 void LevelManager::load_level_2(){
+    /* This will be phased out as we can save a scene to a level */
     LogManager::log_info("No second level yet.", 2);
     const double PI = 3.141592653589793238463;
     const double big_radius = hypermath::radius_for_ngon(PI/3, 12);
@@ -90,3 +90,89 @@ void LevelManager::load_level_2(){
     scene.objects.push_back(eucl_circ_3);
 }
 
+std::vector<object*> LevelManager::parse_lvl(const std::string filename){
+    std::ifstream input("resources/Levels/" + filename + ".lev");
+    std::vector<object*> objects;
+    int iter_num = 0;
+    for(std::string line ; getline(input, line) ; ) {
+        /* Check if the line is a .obj */
+        if(line.find(".obj") != -1){
+            if(line.find(".obj") == 0){
+                LogManager::log_error("You specified an invalid object.");
+                continue;
+            }
+            std::string object_name = line;
+            getline(input, line);
+            double scale = 1.0;
+            if(line.find("Scale:") != -1){
+                line = line.substr(line.find(":")+1);
+                size_t index;
+                scale = std::stod(line, &index);
+            }
+            objects.push_back(new object(("resources/" + object_name).c_str(), false, scale));
+            glm::dmat4 transformation;
+            for(int i = 0; i < 4; i++){
+                getline(input, line);
+                size_t index;
+                transformation[0][i] = std::stod(line, &index);
+                line = line.substr(index);
+                transformation[1][i] = std::stod(line, &index);
+                line = line.substr(index);
+                transformation[2][i] = std::stod(line, &index);
+                line = line.substr(index);
+                transformation[3][i] = std::stod(line, &index);
+            }
+            objects.back()->set_transformation(transformation);
+        }
+        /* Check if the next line is for a .hr file */
+        else if(line.find(".hr") != -1){
+            if(line.find(".hr") == 0){
+                LogManager::log_error("You specified an invalid object.");
+                continue;
+            }
+            objects.push_back(AssetManager::load_object(line.substr(0,line.find(".hr"))));
+            glm::dmat4 transformation;
+            for(int i = 0; i < 4; i++){
+                getline(input, line);
+                size_t index;
+                transformation[0][i] = std::stod(line, &index);
+                line = line.substr(index);
+                transformation[1][i] = std::stod(line, &index);
+                line = line.substr(index);
+                transformation[2][i] = std::stod(line, &index);
+                line = line.substr(index);
+                transformation[3][i] = std::stod(line, &index);
+            }
+            objects.back()->set_transformation(transformation);
+        }
+        /* Check for .stl */
+        else if(line.find(".stl") != -1){
+            LogManager::log_error("Currently only .hr and .obj files are supported. Please run the python converter to turn the .stl into a .hr file."); 
+        }
+        /* Handle comments */
+        else if(line.find("#") != -1){
+            if(iter_num == 0){
+                while(line.find("#")==0){
+                    line = line.substr(1);
+                }                line = line.substr(0, line.find("#"));
+                LogManager::log_info("Loading Level: " + line, 2);
+            }
+            else{
+                while(line.find("#")==0){
+                    line = line.substr(1);
+                }
+                line = line.substr(0, line.find("#"));
+                LogManager::log_info(line, 3);
+            }
+        }
+        iter_num++;
+    }
+    return objects;
+}
+
+void LevelManager::load_level(const char *filename){
+    std::vector<object*> objects = parse_lvl(filename);
+    for(int i=0; i < objects.size(); i++){
+        scene.objects.push_back(objects.at(i));
+    }
+}
--- src/levelmanager/levelmanager.h
+++ src/levelmanager/levelmanager.h
@@ -7,12 +7,13 @@ class LevelManager {
 public:
     static bool startup();
     static void shutdown();
-
     static Scene scene;
+    static void load_level(const char *name);
 
 private:
     static void load_initial_level();
     static void load_level_2();
+    static std::vector<object *> parse_lvl(const std::string filename);
 };
 
 #endif

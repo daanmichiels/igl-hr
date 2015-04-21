609e7c5fb525fec3c1059975d98751848c3b6347
bwooden <barinskajones@comcast.net>
2015-04-20 21:46:25 -0500

    First draft of phong shader implementation

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,9 +1,20 @@
 #version 330
 
 smooth in vec3 theColor;
+smooth in vec4 theNormal;
+
+uniform vec4 lamp;
+uniform vec4 camera;
+uniform int specular;
+uniform int diffuse;
+uniform int ambient;
+uniform float surfaceSpec;
+uniform float surfaceDiff;
+uniform float surfaceAmb;
 
 layout(location = 0) out vec3 color;
 
 void main() {
-    color = (theColor);
+    //color = (theColor);
+	color = surfaceAmb*ambient + surfaceDiff*()
 }
